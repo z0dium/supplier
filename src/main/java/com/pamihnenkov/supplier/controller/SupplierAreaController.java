09b1f4b83e206a0fcce03852af8d9e9f0b1361bd
z0dium <pamihnenkov@gmail.com>
2021-01-23 02:09:39 +0300

    Added supplierArea page and some new methods for test

---
+++ src/main/java/com/pamihnenkov/supplier/controller/SupplierAreaController.java
@@ -0,0 +1,52 @@
+package com.pamihnenkov.supplier.controller;
+
+import com.pamihnenkov.supplier.model.RequestLinesContainer;
+import com.pamihnenkov.supplier.security.ApplicationUser.ApplicationUser;
+import com.pamihnenkov.supplier.security.ApplicationUser.ApplicationUserService;
+import com.pamihnenkov.supplier.service.DepartmentService;
+import com.pamihnenkov.supplier.service.RequestService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.stream.Collectors;
+
+@Controller
+public class SupplierAreaController {
+
+    private final ApplicationUserService applicationUserService;
+    private final RequestService requestService;
+    private final DepartmentService departmentService;
+
+    public SupplierAreaController(ApplicationUserService applicationUserService, RequestService requestService, DepartmentService departmentService) {
+        this.applicationUserService = applicationUserService;
+        this.requestService = requestService;
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping("/supplier")
+    public String showSupplierArea(){
+  //      ApplicationUser currentUser = (ApplicationUser) applicationUserService.loadUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+
+        return "supplierArea";
+    }
+
+    @GetMapping("/supplier/sib")
+    public ModelAndView enterSibArea(Model model){
+
+        ModelAndView mav = new ModelAndView();
+        ApplicationUser currentUser = (ApplicationUser) applicationUserService.loadUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+
+        RequestLinesContainer container = new RequestLinesContainer();
+
+        container.setRequestLines(requestService.findByDepartmentIn(departmentService.findBySupplier(currentUser)).stream()
+                .flatMap(request -> request.getRequestLines().stream())
+                .collect(Collectors.toList()));
+        mav.addObject("container", container);
+        mav.setViewName("allRequestsLines");
+        return mav;
+
+    }
+}
--- src/main/java/com/pamihnenkov/supplier/service/RequestService.java
+++ src/main/java/com/pamihnenkov/supplier/service/RequestService.java
@@ -1,10 +1,14 @@
 package com.pamihnenkov.supplier.service;
 
+import com.pamihnenkov.supplier.model.Department;
 import com.pamihnenkov.supplier.model.Request;
 import com.pamihnenkov.supplier.model.User;
 
 import java.util.List;
+import java.util.Set;
 
 public interface RequestService extends CrudService<Request, Long>{
     List<Request> findByAuthor(User user);
+    List<Request> findByDepartment(Department department);
+    List<Request> findByDepartmentIn(Set<Department> departments);
 }
